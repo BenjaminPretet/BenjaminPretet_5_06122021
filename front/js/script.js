16fd4bbf4ac18c5dbd66d117bce7475c09e0e7fb
BenjaminPretet <bpretet59@gmail.com>
2021-12-13 17:21:46 +0100

    update loop test

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
         if (res.ok) {
@@ -5,8 +6,18 @@ fetch("http://localhost:3000/api/products")
         }
     })
     .then(function(value) {
-        console.log(value);
+        /*appel de la fonction*/
+        displayProduct(value);
     })
     .catch(function(err) {
-
+        console.log(err);
     });
+
+/*déclaration de la fonction*/
+function displayProduct(data){
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        console.log(i);
+    }
+    
+}
