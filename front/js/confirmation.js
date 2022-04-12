404adbb3890d748d2b87aaea7e55c69fc0bd2d5d
Benjamin <bpretet59@gmail.com>
2022-04-12 19:13:33 +0200

    update affichage numéro de commande

---
+++ front/js/confirmation.js
@@ -0,0 +1,7 @@
+//Récupération de l'id de la commande dans le local storage
+const idCommandeConfirme = localStorage.getItem("idConfirmationCommande");
+console.log(`idCommandeConfirme : ${idCommandeConfirme}`);
+
+document.getElementById("orderId").innerHTML = `
+${idCommandeConfirme}
+`
